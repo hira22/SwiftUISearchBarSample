0050c876e6e071bba743e32a53172f4470c1b555
hira22 <m.hiraoka22@gmail.com>
2020-09-09 09:05:01 +0900

    add VGrid

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,9 +10,21 @@ import SwiftUI
 struct ContentView: View {
     @State var text: String = ""
     
+    var columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        SearchBar(text: $text)
         
+        VStack(spacing: 8) {
+            SearchBar(text: $text)
+            
+            ScrollView(.vertical, showsIndicators: true) {
+                LazyVGrid(columns: columns) {
+                    ForEach(items.filter { text.isEmpty || $0.title.contains(text) }) { item in
+                        Text(item.title)
+                    }
+                }
+            }
+        }
     }
     
 }
@@ -22,3 +34,23 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+// MARK: Sample data entity
+struct Item: Identifiable {
+    var id = UUID()
+    var title: String
+}
+
+// MARK: Sample data
+let items = [
+    Item(title: "123"),
+    Item(title: "234"),
+    Item(title: "456"),
+    Item(title: "aaa"),
+    Item(title: "bbb"),
+    Item(title: "this"),
+    Item(title: "is"),
+    Item(title: "test"),
+    Item(title: "data"),
+]
